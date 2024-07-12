f066e52d7bd82b4b1231f56ca295640eb4614c62
Aislan Santos <aislan.santos@gmail.com>
2024-07-12 10:46:58 -0300

    customerController authGuard and RoleGuard

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -4,16 +4,19 @@ import {
 	Delete,
 	Get,
 	Param,
+	ParseIntPipe,
 	Patch,
 	Post,
 	UseGuards
 } from "@nestjs/common";
-import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiConsumes, ApiTags } from "@nestjs/swagger";
+import { AuthGuard } from "../guards/auth/auth.guard";
+import { RoleGuard } from "../guards/role/role.guard";
+import { Roles } from "../utils/decorators/roles.decorator";
+import { Role } from "../utils/enums/role.enum";
 import { CustomersService } from "./customers.service";
 import { CreateCustomerDto } from "./dto/create-customer.dto";
 import { UpdateCustomerDto } from "./dto/update-customer.dto";
-import { AuthGuard } from "../guards/auth/auth.guard";
-import { RoleGuard } from "../guards/role/role.guard";
 
 @ApiTags("Customers")
 @UseGuards(AuthGuard, RoleGuard)
@@ -22,29 +25,44 @@ import { RoleGuard } from "../guards/role/role.guard";
 export class CustomersController {
 	constructor(private readonly customersService: CustomersService) {}
 
+	@Roles(Role.Admin, Role.User)
+	@ApiConsumes("application/x-www-form-urlencoded")
+	@ApiConsumes("application/json")
 	@Post()
 	create(@Body() createCustomerDto: CreateCustomerDto) {
 		return this.customersService.create(createCustomerDto);
 	}
 
+	@Roles(Role.Admin, Role.User)
+	@ApiConsumes("application/x-www-form-urlencoded")
+	@ApiConsumes("application/json")
 	@Get()
 	findAll() {
 		return this.customersService.findAll();
 	}
 
+	@Roles(Role.Admin, Role.User)
+	@ApiConsumes("application/x-www-form-urlencoded")
+	@ApiConsumes("application/json")
 	@Get(":id")
 	findOne(@Param("id") id: string) {
 		return this.customersService.findOne(+id);
 	}
 
+	@Roles(Role.Admin, Role.User)
+	@ApiConsumes("application/x-www-form-urlencoded")
+	@ApiConsumes("application/json")
 	@Patch(":id")
 	update(
-		@Param("id") id: string,
+		@Param("id", ParseIntPipe) id: number,
 		@Body() updateCustomerDto: UpdateCustomerDto
 	) {
-		return this.customersService.update(+id, updateCustomerDto);
+		return this.customersService.update(id, updateCustomerDto);
 	}
 
+	@Roles(Role.Admin, Role.User)
+	@ApiConsumes("application/x-www-form-urlencoded")
+	@ApiConsumes("application/json")
 	@Delete(":id")
 	remove(@Param("id") id: string) {
 		return this.customersService.remove(+id);

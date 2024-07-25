3218b2b3d70f6239d6901433c4ee3b667f079a82
Aislan Santos <aislan.santos@gmail.com>
2024-07-25 15:33:04 -0300

    Implementados os testes, TDD
    Close #16

--- src/branches/branches.service.ts
+++ src/branches/branches.service.ts
@@ -1,26 +1,33 @@
-import { Injectable } from '@nestjs/common';
-import { CreateBranchDto } from './dto/create-branch.dto';
-import { UpdateBranchDto } from './dto/update-branch.dto';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { CreateBranchDto } from "./dto/create-branch.dto";
+import { UpdateBranchDto } from "./dto/update-branch.dto";
+import { BranchEntity } from "./entities/branch.entity";
 
 @Injectable()
 export class BranchesService {
-  create(createBranchDto: CreateBranchDto) {
-    return 'This action adds a new branch';
-  }
+	constructor(
+		@InjectRepository(BranchEntity)
+		private readonly branchRepository: Repository<BranchEntity>
+	) {}
+	public async create(createBranchDto: CreateBranchDto) {
+		return "This action adds a new branch";
+	}
 
-  findAll() {
-    return `This action returns all branches`;
-  }
+	public async findAll() {
+		return await this.branchRepository.find();
+	}
 
-  findOne(id: number) {
-    return `This action returns a #${id} branch`;
-  }
+	public async findOne(id: number) {
+		return `This action returns a #${id} branch`;
+	}
 
-  update(id: number, updateBranchDto: UpdateBranchDto) {
-    return `This action updates a #${id} branch`;
-  }
+	public async update(id: number, updateBranchDto: UpdateBranchDto) {
+		return `This action updates a #${id} branch`;
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} branch`;
-  }
+	public async remove(id: number) {
+		return `This action removes a #${id} branch`;
+	}
 }
